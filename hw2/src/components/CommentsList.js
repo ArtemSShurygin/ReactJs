8b4249b9850129f833779d533045c35865fbd34e
artem <shurtem@mail.ru>
2024-11-14 21:38:47 +0300

    ДЗ 2 сделано.

--- hw2/src/components/CommentsList.js
+++ hw2/src/components/CommentsList.js
@@ -1,16 +1,24 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
-export function CommentsList(params) {
-  const [comments, setComments] = useState([
-    { id: 1, text: "Это первый комментарий" },
-    { id: 2, text: "Это второй комментарий" },
-    { id: 3, text: "Это третий комментарий" }
-    ]);
+export function CommentsList() {
+	const [comments, setComments] = useState([
+		{ id: 1, display: true, text: "Это первый комментарий" },
+		{ id: 2, display: true, text: "Это второй комментарий" },
+		{ id: 3, display: true, text: "Это третий комментарий" },
+	]);
 
-    return comments.map((comment) => <div key={comment.id}>
-      <p>{comment.text}</p>
-      <button>удалить</button>
-      </div>)
+	return comments.map((comment) => (
+		<div key={comment.id} className="comment">
+			<p>{comment.text}</p>
+			<button className="btn-delete-comment"
+				onClick={() => {
+					setComments(comments.filter((thisComment) => thisComment.id !== comment.id));
+				}}
+			>
+				Удалить комментарий
+			</button>
+		</div>
+	));
 }
 
 export default CommentsList;
